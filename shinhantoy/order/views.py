5e33f701b60b95f1ed0ac5fcd120c0e4f0d2d330
yewonbahn <banyaewon98@gmail.com>
2023-01-25 16:03:33 +0900

    feat: create order class

--- shinhantoy/order/views.py
+++ shinhantoy/order/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import mixins, generics,status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from .paginations import OrderLargePagination
+from .models import Order
+from .serializers import (
+    OrderSerializer,)
+
+class OrderListView(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    generics.GenericAPIView
+):
+
+    serializer_class = OrderSerializer
+    pagination_class = OrderLargePagination
+    def get_queryset(self):
+        return Order.objects.all().order_by('id')
+
+    def get(self,request,*args,**kwargs):
+        return self.list(request,args,kwargs)

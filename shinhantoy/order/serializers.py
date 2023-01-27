ea7c839346d948f65462379d474d36e7dc61109d
yewonbahn <banyaewon98@gmail.com>
2023-01-27 09:15:55 +0900

    feat: fix serializers

--- shinhantoy/order/serializers.py
+++ shinhantoy/order/serializers.py
@@ -2,10 +2,6 @@ from rest_framework import serializers
 from .models import Order,Comment
 
 class OrderSerializer(serializers.ModelSerializer):
-    comment_count = serializers.SerializerMethodField();
-
-    def get_comment_count(self,obj):
-        return obj.comment_set.all().count();
     class Meta:
         model = Order
         fields = "__all__"
